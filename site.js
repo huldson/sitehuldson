212df2d3a533b027b70df3d9cb91ea8fe4dd7181
Huldson Richard <huldson-_-@hotmail.com>
2020-10-04 05:55:43 -0300

    incluir a tag h4

--- site.js
+++ site.js
@@ -2,5 +2,8 @@ let button = document.querySelector("#add_task")
 button.addEventListener("click",function(){
     let input = document.querySelector("#task")
     let variavel = input.value
-    console.log(variavel)
+   
+    let lista = document.querySelector("#list")
+    lista.innerHTML = "<br>"+ "<h4>"+input.value+"</h4>" + lista.innerHTML 
+    
 })
